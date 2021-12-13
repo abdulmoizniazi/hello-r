c341467f924f917fe6cf71a23a3a7bec46de98b7
Abdul Moiz Khan <abdulmoizniazi@gmail.com>
2021-12-13 22:56:30 +0500

    001 experiment

--- README.r
+++ README.r
@@ -1,4 +1,13 @@
 # hello-r
-Repo for R programming
+#Repo for R programming
 ### this is an R file
 print ('hello~R')
+
+name = "Moiz"
+Version = "Rstudio~ in writting"
+
+print(name)
+print("these lines are written in R")
+
+
+
